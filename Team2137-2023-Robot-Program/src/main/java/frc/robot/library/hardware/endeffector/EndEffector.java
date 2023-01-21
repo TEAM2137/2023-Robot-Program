994f1bfd42965be86285ccd353aa789de5425a76
[Error_27] <error27dev@gmail.com>
2023-01-21 11:05:12 -0500

    Implement pitch

--- Team2137-2023-Robot-Program/src/main/java/frc/robot/library/hardware/endeffector/EndEffector.java
+++ Team2137-2023-Robot-Program/src/main/java/frc/robot/library/hardware/endeffector/EndEffector.java
@@ -1,18 +1,28 @@
 package frc.robot.library.hardware.endeffector;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.functions.io.FileLogger;
 import frc.robot.functions.io.xmlreader.EntityGroup;
+import frc.robot.functions.io.xmlreader.data.PID;
 import org.w3c.dom.Element;
 
 public class EndEffector extends EntityGroup {
 
     private FileLogger logger;
 
+    private double pitchTarget = 0;
+
     private Solenoid leftJaw;
     private Solenoid rightJaw;
+
+    private CANSparkMax pitchMotor;
+
+    // TODO: PID for pitchMotor
+    private PID pitchPID = new PID(0.25, 0.25, 0.0, "Pitch Motor");
 
     public EndEffector(Element element, int depth, boolean printProcess, FileLogger fileLogger){
         super(element, depth, printProcess, fileLogger);
@@ -21,10 +31,19 @@ public class EndEffector extends EntityGroup {
 
         leftJaw = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
         rightJaw = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+
+        pitchMotor = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        // TODO: PID for pitchMotor
+//        pitchMotor.getPIDController().setP(pitchPID.getP());
+//        pitchMotor.getPIDController().setI(pitchPID.getI());
+//        pitchMotor.getPIDController().setD(pitchPID.getD());
     }
 
     @Override
     public void periodic(){
+        // TODO: Move to pitch logic
+
         // Update dashboard stats
         SmartDashboard.putBoolean("End Effector Closed", getClosed());
     }
@@ -51,6 +70,42 @@ public class EndEffector extends EntityGroup {
     public void setEffectorState(boolean isClosed){
         leftJaw.set(isClosed);
         rightJaw.set(isClosed);
+    }
+
+    /**
+     * Sets the speed of the pitch motor
+     * @param speed Speed of the motor in range from -1 to 1
+     */
+    public void setPitchMotorSpeed(double speed) {
+        pitchMotor.set(speed);
+    }
+
+    /**
+     * Sets the speed of the pitch motor in RPM
+     * @param rpm Speed of the motor in RPM
+     */
+    public void setPitchMotorRPM(double rpm){
+        setPitchMotorSpeed(rpm / 60.0 * 2048 / 10.0);
+    }
+
+    public void setTargetPitch(double degrees, double rpm){
+        pitchTarget = degrees / 360 * 4096;
+    }
+
+    /**
+     * Gets the current pitch in encoder units of the pitch motor
+     * @return Pitch in encoder counts
+     */
+    public double getPitch(){
+        return pitchMotor.getEncoder().getPosition();
+    }
+
+    /**
+     * Gets the current pitch in degrees
+     * @return Pitch in degrees
+     */
+    public double getPitchDegrees(){
+        return getPitch() / 4096 * 360;
     }
 
     /**
