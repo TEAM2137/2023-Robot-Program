5c14e9ab46643cd483c8c47d7b63f259b4b398b8
[Error_27] <error27dev@gmail.com>
2023-03-05 14:16:04 -0500

    XZ Collision
    Doesn't work if the passed body is larger than the collider and has no points inside it.

--- Team2137-2023-Robot-Program/src/main/java/frc/robot/library/Collision.java
+++ Team2137-2023-Robot-Program/src/main/java/frc/robot/library/Collision.java
@@ -26,21 +26,38 @@ public class Collision {
         if(
                 // Is 'body' inside the collider? (Y only)
                 (bodyPoints[0].getYPrimary() >= colliderPoints[0].getYPrimary() &&
-                bodyPoints[0].getYPrimary() <= colliderPoints[3].getYPrimary()) ||
-                (bodyPoints[3].getYPrimary() >= colliderPoints[0].getYPrimary() &&
-                bodyPoints[3].getYPrimary() <= colliderPoints[3].getYPrimary()) ||
+                bodyPoints[0].getYPrimary() <= colliderPoints[4].getYPrimary()) ||
+                (bodyPoints[4].getYPrimary() >= colliderPoints[0].getYPrimary() &&
+                bodyPoints[4].getYPrimary() <= colliderPoints[4].getYPrimary()) ||
 
                 // Is the collider inside 'body' (Y only)
                 (colliderPoints[0].getYPrimary() >= bodyPoints[0].getYPrimary() &&
-                colliderPoints[0].getYPrimary() <= bodyPoints[3].getYPrimary()) ||
-                (colliderPoints[3].getYPrimary() >= bodyPoints[0].getYPrimary() &&
-                colliderPoints[3].getYPrimary() <= bodyPoints[3].getYPrimary())
+                colliderPoints[0].getYPrimary() <= bodyPoints[4].getYPrimary()) ||
+                (colliderPoints[4].getYPrimary() >= bodyPoints[0].getYPrimary() &&
+                colliderPoints[4].getYPrimary() <= bodyPoints[4].getYPrimary())
         ){
-            // TODO: Detect XZ collisions here
-        }else{
-            return false;
-        }
+            // Y CHECK SUCCESS: Check if the shapes overlap on XZ plane
+            for(int i = 0; i < 4; i++){ // Iterate through each point on the base of 'body'
+                double px = bodyPoints[i].getXPrimary();
+                double pz = bodyPoints[i].getZPrimary();
 
-        return false;
+                for (int s = 0; s < 4; s++) { // Check each side of the rectangle
+                    double x1 = colliderPoints[s].getXPrimary();
+                    double x2 = colliderPoints[s > 2 ? 0 : s + 1].getXPrimary();
+
+                    double z1 = colliderPoints[s].getZPrimary();
+                    double z2 = colliderPoints[s > 2 ? 0 : s + 1].getZPrimary();
+
+                    if((x2 - x1) * (pz - z1) - (px - x1) * (z2 - z1) >= 0){
+                        return true; // Point is touching the collider, collision detected
+                    }
+                }
+            }
+
+            return false; // No points collided.
+
+        }else{
+            return false; // Failed Y check; impossible to be colliding.
+        }
     }
 }
